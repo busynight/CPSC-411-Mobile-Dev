96ce03d24d39ef2079f84d0d2b3bdd1c034b9544
Ivan Espinosa <busynight@melissa-espinosas-macbook.local>
2014-08-28 15:48:09 -0700

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Ivan Espinosa on 8/28/14.
+//  Copyright (c) 2014 __CSUF__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
